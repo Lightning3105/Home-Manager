41b460a5b1bbc24fdf8dd3761c70c86e1172ede2
James <james.waters@talktalk.net>
2018-08-28 20:31:15 +0100

    Added the ability to turn the Raspberry Pi screen on and off using HTTP

--- core.py
+++ core.py
@@ -1,9 +1,21 @@
 import flask
 import requests
+import schedule
+from subprocess import Popen
 
 app = flask.Flask(__name__)
 
 app.secret_key = "\x9d\x04/\xc6~\x9e\xea.\xe6\xfa\x89d\xf4M\xb4i\xf8\xf3\x12L\x89GuL"
+
+@app.route('/api/manager/screen/<power>')
+def screen(power):
+	if power in ["on", True, 1]: power = '1'
+	if power in ["off", False, 0]: power = '0'
+	#vcgencmd display_power 0
+	Popen(['vcgencmd', 'display_power', power])
+	return 'Done'
+
+schedule.every().day.at("22:30").do(screen, power='0')
 
 @app.route('/')
 def root():
